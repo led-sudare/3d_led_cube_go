85ad8906c8bf7f4acea0ec29e226f2ad0910a725
Tatsuo Fukushima <tatsuo.fukushima@fxat.co.jp>
2018-10-23 09:07:44 +0900

    cahnge to be ease to accept CORS

--- vendor/ledlib/util/cors_handler.go
+++ vendor/ledlib/util/cors_handler.go
@@ -2,18 +2,16 @@ package util
 
 import "net/http"
 
-func NewCORSHandler(corsAllowed string, handler func(http.ResponseWriter, *http.Request)) http.Handler {
-	return &CORSHandler{corsAllowed, handler}
+func NewCORSHandler(handler func(http.ResponseWriter, *http.Request)) http.Handler {
+	return &CORSHandler{handler}
 }
 
 type CORSHandler struct {
-	corsAllowed string
-	handler     func(http.ResponseWriter, *http.Request)
+	handler func(http.ResponseWriter, *http.Request)
 }
 
 func (c *CORSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", c.corsAllowed)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.handler(w, r)
 }
