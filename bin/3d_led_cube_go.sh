5e66bd9fa0090387ba792502e4f6e3708167264d
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2018-10-31 23:29:17 +0900

    fix the bug app not startup correctly in rpi.

--- bin/3d_led_cube_go.sh
+++ bin/3d_led_cube_go.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 cd /home/pi/go/src/3d_led_cube_go/
-exec ./3d_led_cube_go -r 192.168.0.25:5501 -s show:{\"orders\":[{\"id\":\"object-realsense\",\"lifetime\":0}]}
-
+exec ./3d_led_cube_go -d 192.168.0.10:9001 -r 192.168.0.20:5501 -s \{\"orders\":\[\{\"id\":\"object-realsense\",\"lifetime\":0\}\]\}
